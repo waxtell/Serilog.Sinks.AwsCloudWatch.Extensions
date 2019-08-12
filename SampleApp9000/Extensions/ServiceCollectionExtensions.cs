a8e3a442c773028b0a80bf44137ead0e4e55c985
Will Axtell <waxtell@transtar1.com>
2019-08-12 12:14:11 -0400

    Initial commit

---
+++ SampleApp9000/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.Extensions.NETCore.Setup;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SampleApp9000.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        // ReSharper disable once InconsistentNaming
+        public static IServiceCollection AddDefaultAWSOptions(this IServiceCollection collection)
+        {
+            collection
+                .Add
+                (
+                    new ServiceDescriptor
+                    (
+                        typeof(AWSOptions),
+                        provider => provider
+                            .GetService<IConfiguration>()
+                            .GetAWSOptions(),
+                        ServiceLifetime.Singleton
+                    )
+                );
+
+            return collection;
+        }
+    }
+}
