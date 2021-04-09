b59e55f4572f46d73ebdfca137dca2bcfe9809bf
Will Axtell <waxtell@transtar1.com>
2021-04-09 16:25:55 -0400

    Removed hosted service and credentials file.  Updated to 3.5.x AWS dependencies.

---
+++ SampleApp9000/AwsAppSettingsCredentials.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.Runtime;
+
+namespace SampleApp9000
+{
+    public class AwsAppSettingsCredentials : AWSCredentials
+    {
+        public string AccessKeyId { get; set; }
+        public string SecretKey { get; set; }
+
+        public override ImmutableCredentials GetCredentials()
+        {
+            return
+                new ImmutableCredentials
+                (
+                    AccessKeyId,
+                    SecretKey,
+                    null
+                );
+        }
+    }
+}
---
+++ SampleApp9000/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static async Task RunConsoleAsync<T>(this IHost host, Func<T, Task> worker)
+        {
+            await worker.Invoke(host.Services.GetService<T>());
+        }
+    }
+}
